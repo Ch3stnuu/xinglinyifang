5073ca92e9518c3e8566303d7ee6a13a6626c6c4
heeeyou <741234559@qq.com>
2018-07-18 16:36:14 +0800

    CRE ADD
    CRE
    创建model包及其中的相关类
    
    ADD
    MyConfig.java中，添加有关数据库连接的配置

--- src/main/java/cn/nt/xinglinyifang/common/MyConfig.java
+++ src/main/java/cn/nt/xinglinyifang/common/MyConfig.java
@@ -1,7 +1,11 @@
 package cn.nt.xinglinyifang.common;
 
+import cn.nt.xinglinyifang.model.*;
 import com.jfinal.config.*;
+import com.jfinal.kit.PathKit;
 import com.jfinal.kit.PropKit;
+import com.jfinal.plugin.activerecord.ActiveRecordPlugin;
+import com.jfinal.plugin.druid.DruidPlugin;
 import com.jfinal.template.Engine;
 
 public class MyConfig extends JFinalConfig {
@@ -24,6 +28,26 @@ public class MyConfig extends JFinalConfig {
 
     @Override
     public void configPlugin(Plugins me) {
+        //druid 数据源插件
+        DruidPlugin dp = new DruidPlugin(PropKit.get("url"),PropKit.get("username"),PropKit.get("password"));
+        dp.setDriverClass(PropKit.get("driverClass"));
+        dp.set(PropKit.getInt("initialSize"), PropKit.getInt("minIdle"), PropKit.getInt("maxActive"));
+        dp.setMaxWait(PropKit.getInt("maxWait"));
+        me.add(dp);
+        //ARP插件
+        ActiveRecordPlugin arp = new ActiveRecordPlugin(dp);
+        me.add(arp);
+        arp.setBaseSqlTemplatePath(PathKit.getWebRootPath() + "/WEB-INF");
+        arp.addSqlTemplate("sql/all.sql");
+        arp.addMapping("activation", Activation.class);
+        arp.addMapping("disease", Disease.class);
+        arp.addMapping("doctor", Doctor.class);
+        arp.addMapping("medicalCase", MedicalCase.class);
+        arp.addMapping("medicine", Medicine.class);
+        arp.addMapping("Picture", Picture.class);
+        arp.addMapping("relationship", Relationship.class);
+        arp.addMapping("technology", Technology.class);
+        arp.addMapping("video", Video.class);
 
     }
 
