231a4f2e3e3bc33ab165a31d0b346a8476041f0d
heeeyou <741234559@qq.com>
2018-07-19 10:24:25 +0800

    CRE ADD
    CRE
    创建动态sql文件
    
    ADD
    pom.xml中添加druid的依赖

---
+++ src/main/webapp/WEB-INF/sql/all.sql
@@ -0,0 +1,35 @@
+#namespce("activation")
+  #include("activation.sql")
+#end
+
+#namespace("disease")
+  #inclide("disease.sql")
+#end
+
+#namespace("doctor")
+  #inclide("doctor.sql")
+#end
+
+#namespace("medicalCase")
+  #inclide("medicalCase.sql")
+#end
+
+#namespace("medicine")
+  #inclide("medicine.sql")
+#end
+
+#namespace("picture")
+  #inclide("picture.sql")
+#end
+
+#namespace("relationship")
+  #inclide("relationship.sql")
+#end
+
+#namespace("technology")
+  #inclide("technology.sql")
+#end
+
+#namespace("video")
+  #inclide("video.sql")
+#end
